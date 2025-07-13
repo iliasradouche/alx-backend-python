6d0a1e6a7f20e913dd62c4d7f1eff9467ede9fc8
iliasradouche <iliasradouche2@gmail.com>
2025-07-13 16:26:28 +0100

    Task 1 - 2

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+class ExecuteQuery:
+    def __init__(self, query, params=None):
+        self.query = query
+        self.params = params or ()
+        self.conn = None
+        self.cursor = None
+        self.results = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect('users.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        self.results = self.cursor.fetchall()
+        return self.results
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+
+query = "SELECT * FROM users WHERE age > ?"
+params = (25,)
+
+with ExecuteQuery(query, params) as results:
+    print(results)
